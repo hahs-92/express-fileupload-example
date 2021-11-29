ae1ba501b1ac3a63250dc9ac5b5f81bd5ab17c72
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-11-29 16:01:00 -0500

    upload image with mv fucntion

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+const fileUpload = require('express-fileupload')
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
+
+//default option
+app.use(fileUpload())
 
 //template engine
 app.engine('.hbs', engine({ extname: '.hbs'}))
@@ -13,6 +16,26 @@ app.set('views', './views')
 //routes
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+
+app.post('/', (req, res) => {
+    let sampleFile, uploadPath
+
+    if(!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded')
+    }
+
+    //name of the input is sampleFile
+    sampleFile = req.files.sampleFile
+    uploadPath = __dirname + '/upload/' + sampleFile.name
+
+    //use mv() to place file on the server
+    sampleFile.mv(uploadPath, function(err) {
+        if(err) return res.status(500).send(err.message)
+
+        res.send('File uploaded!')
+    })
 })
 
 app.listen(PORT, () => {
